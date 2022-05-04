0da006b628cda1e3253fd3b5b9b4d098f0bb7dcd
Ruby Tseng <ytseng7@ucsc.edu>
2022-05-03 19:19:30 -0700

    creative tilt added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/*For our game we wanted to create a cute, simple endless runner. To amplify our game a bit we decided to make one of 
+the obstacles a plastic bag to create a bit of social commentary on pollution. We implemented enemies such as fishing 
+rod, and eels, which are dependent on the position of the enemies. Fishing rod also has a pause before moving back up. 
+While we didn’t use this specific function in game (because it ends up looking buggy), our programmer learned to check
+for adjacency of objects to make sure obstacles are not colliding or congesting. 
+For the audio we learned how to use Bosca Ceoil to create a looping soundtrack imitating other ocean themed pixel 
+games. Sound effects were all crafted on our own and edited using Reaper and the spitfire audio extension.
+For the art, multiple art programs were used to create the visual assets. There were some issues with the transparent 
+plastic bag which took some creativity to crop and edit. Many reference pictures of sea creatures were pulled up to 
+create the sprites with the goal of making the animals recognizable and a bit realistic.
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
