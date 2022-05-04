d12845bfda38ccc33eae7e9fdbc914cb7d5c7727
emeryplyler <emery.plyler@gmail.com>
2022-05-03 23:20:24 -0700

    title texture fixed

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,16 +11,18 @@ class Play extends Phaser.Scene{
         this.load.image('bag', './assets/plasticbag2.png');
         this.load.image('hook', './assets/hook2.png');
         // load animations
-        this.load.spritesheet('shrimpswim', './assets/shrimpanimated.png',
-                    {frameWidth: 100, frameHeight: 75, startFrame: 0, endFrame: 1});
+        this.load.spritesheet('shrimpswim', './assets/shrip.png',
+                    {frameWidth: 100, frameHeight: 75, startFrame: 0, endFrame: 2});
         this.load.spritesheet('eelwiggle', './assets/eelanimated.png', 
-                    {frameWidth: 87.5, frameHeight: 250, startFrame: 0, endFrame: 1});
+                    {frameWidth: 128, frameHeight: 376, startFrame: 0, endFrame: 1});
         this.load.spritesheet('trashfloat', './assets/plasticbag.png', 
-                    {frameWidth: 100, frameHeight: 100, startFrame: 0, endFrame: 1});
+                    {frameWidth: 80, frameHeight: 89, startFrame: 0, endFrame: 1});
         this.load.spritesheet('hookup', './assets/hook.png', 
                     {frameWidth: 35, frameHeight: 250, startFrame: 0, endFrame: 1});
         this.load.spritesheet('fishswim', './assets/fish.png', 
-                    {frameWidth: 100, frameHeight: 100, startFrame: 0, endFrame: 1});
+                    {frameWidth: 180, frameHeight: 82, startFrame: 0, endFrame: 1});
+        this.load.spritesheet('shrimpdie', './assets/shripdie.png', 
+                    {frameWidth: 100, frameHeight: 75, startFrame: 0, endFrame: 7});
     }
 
     create(){
@@ -40,7 +42,7 @@ class Play extends Phaser.Scene{
         // set up animations
         this.anims.create({
             key: 'shrimpmove',
-            frames: this.anims.generateFrameNumbers('shrimpswim', {start: 0, end: 1, first: 0}),
+            frames: this.anims.generateFrameNumbers('shrimpswim', {start: 0, end: 2, first: 0}),
             frameRate: 4,
             repeat: -1
         });
@@ -67,6 +69,11 @@ class Play extends Phaser.Scene{
             frames: this.anims.generateFrameNumbers('fishswim', {start: 0, end: 1, first: 0}),
             frameRate: 4,
             repeat: -1
+        });
+        this.anims.create({
+            key: 'shrimpdeath',
+            frames: this.anims.generateFrameNumbers('shrimpdie', {start: 0, end: 7, first: 0}),
+            frameRate: 9
         });
 
         this.pShrimp = new Shrimp(this, game.config.width-borderPadding-borderUISize, game.config.height/2, 'shrimp').setOrigin(1, 0);
@@ -96,15 +103,11 @@ class Play extends Phaser.Scene{
 
         this.scoreLeft = this.add.text(borderUISize+borderPadding, borderUISize+borderPadding*2, this.point, scoreConfig);
 
-        if(this.gameOver){
-            //gameover screen
-        }
+        // if(this.gameOver){
+        //     //gameover screen
+        // }
     }
     update(){
-        if(this.gameOver){
-            game.config.point = this.point;
-            this.scene.start("creditScene");
-        }
         if(!this.gameOver){
             this.bg.tilePositionX -= this.speed;
             this.fg.tilePositionX -= this.speed * 2;
@@ -128,17 +131,22 @@ class Play extends Phaser.Scene{
         }
         if(this.checkCollision(this.pShrimp, this.rockObs)){
             this.sound.play('chomp');
+            // this.shrimpDeath(this.pShrimp);
             this.gameOver = true;
         }if(this.checkCollision(this.pShrimp, this.eel)){
             this.sound.play('shock');
+            // this.shrimpDeath(this.pShrimp);
             this.gameOver = true;
         }if(this.checkCollision(this.pShrimp, this.bag)){
             this.sound.play('trash');
+            // this.shrimpDeath(this.pShrimp);
             this.gameOver = true;
         }if(this.checkCollision(this.pShrimp, this.rod)){
             this.sound.play('fishing');
+            // this.shrimpDeath(this.pShrimp);
             this.gameOver = true;
         }
+        
         /*if(this.checkAdj(this.eel, this.rockObs)){
             this.rockObs.reset();
         }if(this.checkAdj(this.eel, this.bag)){
@@ -146,7 +154,11 @@ class Play extends Phaser.Scene{
         }if(this.checkAdj(this.bag, this.rockObs)){
             this.rockObs.reset();
         }*/
-        
+        if(this.gameOver == true){
+            
+            game.config.point = this.point;
+            this.scene.start("creditScene");
+        }
     }
 
     checkCollision(shrimp, obs){
@@ -168,5 +180,20 @@ class Play extends Phaser.Scene{
         }else{
             return false;
         }
+    }
+    // just plays death animation at shrimp's location
+    shrimpDeath(shrimp) {
+        // temporarily hide shrimp
+        shrimp.alpha = 0;
+        // play death animation on location
+        let die = this.add.sprite(shrimp.x, shrimp.y, 'shrimpdie').setOrigin(0, 0); // create a sprite on location
+        die.anims.play('shrimpdeath'); // have sprite play the animation
+        die.on('animationcomplete', () => {
+            shrimp.reset();
+            shrimp.alpha = 1;
+            die.destroy();
+            this.gameOver = true;
+        });
+        
     }
 }
